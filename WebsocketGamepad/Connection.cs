8b4aadc41604073fa19c8246e0a46780090bd259
Bacon <baconprime@users.noreply.github.com>
2015-01-11 16:41:52 -0800

    Whoops, forgot this one.

---
+++ WebsocketGamepad/Connection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebsocketGamepad
+{
+    public class Connection
+    {
+        public string UserId;
+        public string Address;
+        public bool Active;
+        public bool Banned;
+
+        public override string ToString()
+        {
+            return Active.ToString() + "\t" + Address + "\t" + UserId;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return UserId == ((Connection)obj).UserId;
+        }
+    }
+}
